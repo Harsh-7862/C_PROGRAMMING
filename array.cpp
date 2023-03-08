a77cb0fc6f1c8644c3824cfac1e0fe97a7e892ab
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-08 12:59:00 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+float percentage[]={56.4,99.7,12.29,67.3};
+for(int i=0;i<4;i++){
+cout<<"Element "<<i+1<<" = "<<percentage[i]<<endl;
+
+}
+return 0;
+}
