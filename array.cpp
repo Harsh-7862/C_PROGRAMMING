1cdde3e860aeac063b885b7e1ee1b88feb8017e7
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-08 13:00:36 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 float percentage[]={56.4,99.7,12.29,67.3};
+cout<<"Following list is for array printing of all elements"<<endl;
 for(int i=0;i<4;i++){
 cout<<"Element "<<i+1<<" = "<<percentage[i]<<endl;
 
