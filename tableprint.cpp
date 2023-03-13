10e933d85ba3dc600e1dcab7b47851cba8a5e58e
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-13 21:46:32 +0530

    Update tableprint.cpp

--- tableprint.cpp
+++ tableprint.cpp
@@ -5,8 +5,8 @@ int n;
   cout<<"Enter the value for printing the table of entered value :";
   cin>>n;
   cout<<endl;
-  for(i=1;i<=n;i++){
-  for(j=1;j<=i;j++)
+  for(int i=1;i<=n;i++){
+  for(int j=1;j<=i;j++)
   {
   cout<<i*n;
   }cout<<endl;
