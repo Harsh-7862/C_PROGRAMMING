74ace2f2ef49d7d60b5fd66631812c46db862d9c
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-13 21:43:28 +0530

    Create tableprint.cpp

---
+++ tableprint.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+  cout<<"Enter the value for printing the table of entered value :";
+  cin>>n;
+  cout<<endl;
+  for(i=1;i<=n;i++){
+  for(j=1;j<=i;j++)
+  {
+  cout<<i*n;
+  }cout<<endl;
+  }
+  
+return 0;
+}
