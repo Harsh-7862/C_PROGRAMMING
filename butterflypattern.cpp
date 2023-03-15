c5250723fd340cc26957987f50986b14112ff1ce
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-15 15:45:33 +0530

    to print butterfly pattern *

---
+++ butterflypattern.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n=6;
+   for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n;j++)
+    {
+        if(i>=j)
+        {
+            cout<<"*";
+        }
+        else
+        {
+            cout<<" ";
+        }
+    }
+    for(int k=0;k<n;k++)
+    {
+        if((i+k)>=(n-1))
+        {
+            cout<<"*";
+        }
+        else
+        {
+            cout<<" ";
+        }
+    }cout<<endl;
+}
+   
+ for(int i=1;i<n;i++)  {
+    for(int j=0;j<n;j++)
+    {
+        if((i+j)<=(n-1))
+        {
+            cout<<"*";
+        }
+        else
+        {
+            cout<<" ";
+        }
+    }
+    for(int k=0;k<n;k++)
+    {
+        if(k>=i)
+        {
+            cout<<"*";
+        }
+        else
+        {
+            cout<<" ";
+        }
+    }
+    cout<<endl;
+}
+    return 0;
+}
