43cefdf5efe3f835025cfb2d6d4a1b370cd2bd90
harshdubey1198 <amanad08052@gmail.com>
2023-03-17 16:22:14 +0530

    codefor

---
+++ ques.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class A
+{ 
+               public:
+     int *p;
+
+    A(){
+        p= new int;
+    }
+    void show(){cout<<"Data : "<<*p<<endl;}
+};
+
+// class B : public A
+// { public:
+
+
+// };
+
+
+int main()
+{
+
+    
+A ob1;
+*(ob1.p)=25;
+ob1.show();
+A ob2 = ob1;
+ob2.show();
+   // return 0;
+}
+          
