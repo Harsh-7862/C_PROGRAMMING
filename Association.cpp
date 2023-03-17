a9a85f2062fde517f7dafc0e09fbe966e66dec20
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-17 11:44:37 +0530

    Add files via upload

---
+++ Association.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void swap(int *x,int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+
+int main(){
+    int x=10,y=20;
+   //swap(x,y);
+    swap(&x,&y);
+cout <<x<<endl<< y<<endl;
+
+    return 0;
+}
