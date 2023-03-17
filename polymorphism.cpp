90c567da61deefb40ce72866e1ab7a9231d9b4a6
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-17 12:12:57 +0530

    Update polymorphism.cpp

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,3 +1,10 @@
-// polymorphism
+// polymorphism-> C++ is when the behavior of the same object or function is different in different contexts.
 // 1.static compile time -> a.function/ method overloading. -> b. operator overloading. ->c. constructor overloading
 // 2.Dynamic runtime polymorphism.
+// COMPILE-TIME	
+// Compile-time polymorphism is also known as static or early binding polymorphism.	
+// The function calls are resolved by the compiler.	
+ 
+//   RUN-TIME
+// Run-time polymorphism is also known as dynamic or late binding polymorphism.
+// The function calls are not resolved by the compiler.
