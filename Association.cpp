9b0e374674adac77b91c5b76e14ae9630174eae6
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-17 11:49:31 +0530

    Update Association.cpp

--- Association.cpp
+++ Association.cpp
@@ -16,3 +16,28 @@ cout <<x<<endl<< y<<endl;
 
     return 0;
 }
+
+
+
+//call by reference
+
+
+#include<iostream>
+using namespace std;
+void swap(int &x,&y){
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+
+
+int main(){
+    int x=10,y=20;
+   swap(x,y);
+   // swap(&x,&y);
+cout <<x<<endl<< y<<endl;
+
+    return 0;
+}
+
