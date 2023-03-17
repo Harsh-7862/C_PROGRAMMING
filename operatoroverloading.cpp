9895d1d02300f71bbd106f2c7e42271c11a30868
HARSH DUBEY <79989737+harshdubey1198@users.noreply.github.com>
2023-03-17 14:38:38 +0530

    Update operatoroverloading.cpp

--- operatoroverloading.cpp
+++ operatoroverloading.cpp
@@ -1,4 +1,46 @@
-//code for operator overloading.
+// //code for operator overloading.
+// #include<iostream>
+// using namespace std;
+// class Demo
+// {
+// private:
+// int a;
+// int b;
+// public:
+//  void setA(int x){a=x;}
+//  int getA(){return a;}
+//  void setB(int y){b=y;}
+//  int getB(){return b;}
+//  void show(){cout<<"a = "<<a <<"& b = "<<b<<endl;}
+//  Demo operator +(const Demo & ob2);
+// };
+// Demo Demo:: operator +(const Demo &ob2){
+// Demo temp;
+// temp.a=a+ob2.a;
+// temp.b=b+ob2.b;
+// return temp;
+// }
+//  int main(){
+//     Demo ob1;
+//     Demo ob2;
+//     Demo res;
+//         ob1.setA(15);
+//         ob1.setB(25);
+//         ob1.show();
+        
+//         ob2.setA(12);
+//         ob2.setB(27);
+//         ob2.show();
+//           cout<<endl<<"a=a1+a2 & b=b1+b2"<<endl;
+//         res =ob1 + ob2; //res=ob1.operator +(ob2)// overloaded assignment operator is used. 
+//         res.show();
+//     return 0;
+// }
+
+//code 2 for preincrement operator
+//++ob;//ob.operator++( );
+
+
 #include<iostream>
 using namespace std;
 class Demo
@@ -13,14 +55,19 @@ public:
  int getB(){return b;}
  void show(){cout<<"a = "<<a <<"& b = "<<b<<endl;}
  Demo operator +(const Demo & ob2);
+ void operator ++();
 };
-
 Demo Demo:: operator +(const Demo &ob2){
 Demo temp;
 temp.a=a+ob2.a;
 temp.b=b+ob2.b;
 return temp;
+}
 
+void Demo:: operator ++()
+{
+++a;
+++b;
 }
  int main(){
     Demo ob1;
@@ -35,6 +82,8 @@ return temp;
         ob2.show();
           cout<<endl<<"a=a1+a2 & b=b1+b2"<<endl;
         res =ob1 + ob2; //res=ob1.operator +(ob2)// overloaded assignment operator is used. 
+        res.show();
+        ++res;
         res.show();
     return 0;
 }
