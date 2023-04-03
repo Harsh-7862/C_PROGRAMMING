7476780b07cd345c55bd04df79294271c491d3d2
harshdubey1198 <amanad08052@gmail.com>
2023-04-03 10:44:16 +0530

    some of the codes

--- whileLoop.cpp
+++ whileLoop.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-int i=5;                        //if int i=0
+int i=20;                      //if int i=0
 while(i>0){                     //if i<=5
     cout<<i<<endl;
     i--;                        //if i++ then output will be 1 \n 2 \n 3 \n 4 \n 5
+
+
 }
 
 
