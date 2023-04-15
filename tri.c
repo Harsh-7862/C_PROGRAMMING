46ecd4f98daa0c19f3b07730c52a7cd681c8480f
harshdubey1198 <amanad08052@gmail.com>
2023-04-15 10:11:43 +0530

    training q's

---
+++ tri.c
@@ -0,0 +1,54 @@
+//code 1
+// #include<stdio.h>
+// int main(){
+//     int sum =2+4/2+6*2;
+//     printf("%d",sum);
+//     return 0;
+// }
+
+
+
+
+//code 2
+// #include <stdio.h>
+// void solve()
+// {
+//     int a[] = {1, 2, 3, 4, 5};
+//     int sum = 0;
+//     for (int i = 0; i < 5; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             sum += *(a + i);
+//         }
+//         else{
+//             sum-=*(a+i);
+//         }}printf("%d",sum);
+    
+// }
+// int main(){
+//     solve();
+//     return 0;
+// }
+
+
+//code 3
+// #include<stdio.h>
+// void solve(){
+// int first=10,second=20;
+// int third=first+second;
+// {
+// int third=second-first;
+// printf("%d ",third);
+// }
+// printf("%d",third);
+// }
+// int main(){
+
+// solve();
+//     return 0;
+// }
+
+
+
+
