68abd190ae8fd36041f8e928e6a0cdb93b0acaad
harshdubey1198 <amanad08052@gmail.com>
2023-04-16 10:45:35 +0530

    day 3 code 2

--- trainingzohaib.cpp
+++ trainingzohaib.cpp
@@ -1,20 +1,50 @@
-#include<iostream>
+// code 1
+// #include<iostream>
+// using namespace std;
+
+
+// int Divisible(int n, int x, int y,int z){
+// int count=0;
+// for(int i=1;i<=n;i++){
+// if(i%x==0 && i%y == 0 && i%z==0)
+// count++;
+// }
+
+//     return count;
+// }
+
+// int main(){
+// int x,y,z,n;
+// cin>>n>>x>>y>>z;
+// cout<<Divisible(n,x,y,z)<<endl;
+//     return 0;
+// }
+#include <iostream>
+
 using namespace std;
 
+double sum_of_avSub(int arr[], int n) {
+    double total_sum = 0;
 
-int Divisible(int n, int x, int y){
-int count=0;
-for(int i=1;i<=n;i++){
-if(i%x==0 && i%y == 0)
-count++;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int subarray_sum = 0;
+            for (int k = i; k <= j; k++) {
+                subarray_sum += arr[k];
+            }
+            int subarray_len = j - i + 1;
+            double subarray_avg = (double) subarray_sum / subarray_len;
+            total_sum += subarray_avg;
+        }
+    }
+
+    return total_sum;
 }
 
-    return count;
-}
-
-int main(){
-int x,y,n;
-cin>>n>>x>>y;
-cout<<Divisible(n,x,y)<<endl;
+int main() {
+    int arr[] = {5, 6, 1,2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    double result = sum_of_avSub(arr, n);
+    cout << "Sum of average of all subarrays is: " << result << endl;
     return 0;
 }
